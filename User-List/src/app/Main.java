b367667fdf0be02b29370e6032e037fa5248e4da
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-04-06 16:32:27 +0200

    Add Scanner

--- User-List/src/app/Main.java
+++ User-List/src/app/Main.java
@@ -1,9 +1,20 @@
 package app;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        UserRepository userRepository = new UserRepository();
+        userRepository.addUser(new User(1, "Alice", "alice@example.com"));
+        userRepository.addUser(new User(2, "Mike", "mike@example.com"));
+        userRepository.addUser(new User(3, "Cat", "cat@example.com"));
 
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter user id: ");
+        int id = scanner.nextInt();
+        Optional<User> user = userRepository.findUserById(id);
 
     }
 }

a2351ac0b032e9a98485e9657f94c6b356b1acf0
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-04-06 16:12:53 +0200

    Modify Method findUserByid

--- User-List/src/app/UserRepository.java
+++ User-List/src/app/UserRepository.java
@@ -1,17 +1,23 @@
 package app;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepository {
+
     List<User> userList = new ArrayList<>();
 
     public Optional<User> findUserById(int id) {
-        return userList.stream()
-                .filter(user -> user.getId() == id)
+        Optional<User> user = userList.stream()
+                .filter(u -> u.getId() == id)
                 .findFirst();
-
+        user.ifPresentOrElse(
+                u -> System.out.println("User id is " + u.getId()),
+                () -> System.out.println("User id is not found")
+        );
+        return user;
     }
-
 }
